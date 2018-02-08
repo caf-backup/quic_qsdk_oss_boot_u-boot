caf-backup/quic_qsdk_oss_boot_u-boot

commit 2e4968663d1a4af27105d18c9f21c10b1fb90ba6

sysupgrade: Add support to authenticate image using TZ
This change will look for existence of file /sys/sec_upgrade/sec_auth
if the file exists, it performs image authentication with the new
approach i.e TZ enabled mechanism.

It reads all the sections of the single image and writes each
section's type and image file path in /sys/sec_upgrade/sec_auth.
If a written section image is a proper signed image, then it will
return success else will cause the failure.

Change-Id: I649581e0ab74a66d677e5bfbf1c34fd83cb2465d
Signed-off-by: Avinash Pandey <avinasv@codeaurora.org>

tools/sysupgrade.c
@@ -27,6 +27,7 @@
 #define RPM_VERSION_FILE       "rpm_version"
 #define VERSION_FILE_BASENAME  "/sys/devices/system/qfprom/qfprom0/"
 #define AUTHENTICATE_FILE	"/sys/devices/system/qfprom/qfprom0/authenticate"
+#define SEC_AUTHENTICATE_FILE  "/sys/sec_upgrade/sec_auth"
 #define TEMP_KERNEL_PATH	"/tmp/tmp_kernel.bin"
 #define MAX_SBL_VERSION	11
 #define MAX_HLOS_VERSION	32
@@ -56,6 +57,7 @@ struct image_section sections[] = {
 		.file			= TMP_FILE_DIR,
 		.version_file		= APPSBL_VERSION_FILE,
 		.is_present		= NOT_PRESENT,
+		.img_code		= "0x9"
 	},
 	{
 		.section_type		= HLOS_TYPE,
@@ -64,6 +66,7 @@ struct image_section sections[] = {
 		.file			= TMP_FILE_DIR,
 		.version_file		= HLOS_VERSION_FILE,
 		.is_present		= NOT_PRESENT,
+		.img_code		= "0x17"
 	},
 	{
 		.section_type		= HLOS_TYPE,
@@ -74,6 +77,7 @@ struct image_section sections[] = {
 		.file			= TEMP_KERNEL_PATH,
 		.version_file		= HLOS_VERSION_FILE,
 		.is_present		= NOT_PRESENT,
+		.img_code		= "0x17"
 	},
 	{
 		.section_type		= TZ_TYPE,
@@ -82,6 +86,7 @@ struct image_section sections[] = {
 		.file			= TMP_FILE_DIR,
 		.version_file		= TZ_VERSION_FILE,
 		.is_present		= NOT_PRESENT,
+		.img_code		= "0x7"
 	},
 	{
 		.section_type		= SBL_TYPE,
@@ -90,6 +95,7 @@ struct image_section sections[] = {
 		.file			= TMP_FILE_DIR,
 		.version_file		= SBL_VERSION_FILE,
 		.is_present		= NOT_PRESENT,
+		.img_code		= "0x0"
 	},
 	{
 		.section_type		= SBL_TYPE,
@@ -98,6 +104,7 @@ struct image_section sections[] = {
 		.file			= TMP_FILE_DIR,
 		.version_file		= SBL_VERSION_FILE,
 		.is_present		= NOT_PRESENT,
+		.img_code		= "0x5"
 	},
 	{
 		.section_type		= SBL_TYPE,
@@ -106,6 +113,7 @@ struct image_section sections[] = {
 		.file			= TMP_FILE_DIR,
 		.version_file		= SBL_VERSION_FILE,
 		.is_present		= NOT_PRESENT,
+		.img_code		= "0x6"
 	},
 	{
 		.section_type		= RPM_TYPE,
@@ -114,6 +122,7 @@ struct image_section sections[] = {
 		.file			= TMP_FILE_DIR,
 		.version_file		= RPM_VERSION_FILE,
 		.is_present		= NOT_PRESENT,
+		.img_code		= "0xA"
 	},
 };
 
@@ -204,6 +213,44 @@ int get_sections(void)
 	return 1;
 }
 
+int load_sections(void)
+{
+	DIR *dir;
+	int i;
+	struct dirent *file;
+	struct image_section *sec;
+
+	dir = opendir(TMP_FILE_DIR);
+	if (dir == NULL) {
+		printf("Error accessing the %s image directory\n", TMP_FILE_DIR);
+		return 0;
+	}
+
+	while ((file = readdir(dir)) != NULL) {
+		for (i = 0, sec = &sections[0]; i < NO_OF_SECTIONS; i++, sec++) {
+			if (strstr(file->d_name, sec->type)) {
+				if (sec->pre_op) {
+					strlcat(sec->tmp_file, file->d_name,
+							sizeof(sec->tmp_file));
+					if (!sec->pre_op(sec)) {
+						printf("Error extracting %s from ubi\n",
+									   sec->tmp_file);
+						closedir(dir);
+						return 0;
+					}
+				} else {
+					strlcat(sec->file, file->d_name,
+							sizeof(sec->file));
+				}
+				sec->is_present = PRESENT;
+				break;
+			}
+		}
+	}
+	closedir(dir);
+	return 1;
+}
+
 /**
  * is_authentication_check_enabled() - checks whether installed image is
  * secure(1) or not(0)
@@ -227,6 +274,17 @@ int is_authentication_check_enabled(void)
 		return 0;
 	}
 
+	return 1;
+}
+
+int is_tz_authentication_enabled(void)
+{
+	struct stat sb;
+
+	if (stat(SEC_AUTHENTICATE_FILE, &sb) == -1) {
+		perror("stat");
+		return 0;
+	}
 	return 1;
 }
 
@@ -1169,9 +1227,43 @@ int is_image_authenticated(void)
 	return 1;
 }
 
+int sec_image_auth()
+{
+	int fd, i, len;
+	char buf[256];
+
+	fd = open(SEC_AUTHENTICATE_FILE, O_RDWR);
+	if (-1 == fd) {
+		perror(SEC_AUTHENTICATE_FILE);
+		return 1;
+	}
+
+	for (i = 0; i < NO_OF_SECTIONS; i++) {
+		if (!sections[i].is_present) {
+			continue;
+		}
+
+		len = snprintf(buf, sizeof(buf), "%s %s", sections[i].img_code, sections[i].file);
+		if (write(fd, buf, len) != len) {
+			perror("write");
+			printf("%s Image authentication failed\n", buf);
+			return 1;
+		}
+	}
+	close(fd);
+	return 0;
+}
+
 int do_board_upgrade_check(char *img)
 {
-	if (is_authentication_check_enabled()) {
+	if (is_tz_authentication_enabled()) {
+		printf("TZ authentication enabled ...\n");
+		if (!load_sections()) {
+			printf("Error: Failed to load sections from image: %s\n", img);
+			return 1;
+		}
+		return sec_image_auth();
+	} else if (is_authentication_check_enabled()) {
 		if (!get_sections()) {
 			printf("Error: %s is not a signed image\n", img);
 			return 1;

tools/sysupgrade.h
@@ -24,6 +24,7 @@ struct image_section {
 	int local_version;
 	char *version_file;
 	int is_present;
+	char *img_code;
 	int (*pre_op)(struct image_section *);
 	int (*get_sw_id)(struct image_section *);
 	int (*split_components)(struct image_section *, char **, char**, char**);

commit 56f1ade45dcd9d86c03a5eb9c57b3e7bd3269546

sysupgrade: close the file if write fails
In previous commit 2e49686, we missed to close the 
file if write () function fails during writing image 
type and image name to sysfs file. This change adds 
the code to close the file.

Change-Id: I5a7a1bdf08ccab3ddb084cb2dba9ac8ab3a0ab65
Signed-off-by: Avinash Pandey <avinasv@codeaurora.org>

tools/sysupgrade.c
@@ -1258,6 +1258,7 @@ int sec_image_auth()
 		len = snprintf(buf, sizeof(buf), "%s %s", sections[i].img_code, sections[i].file);
 		if (write(fd, buf, len) != len) {
 			perror("write");
+			close(fd);
 			printf("%s Image authentication failed\n", buf);
 			return 1;
 		}